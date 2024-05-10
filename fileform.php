7a09f6e824fdfc2d307f6d9b8f12cd456bf1251c
Carylle Go <caryllemariego@gmail.com>
2024-05-10 10:02:01 +0800

    fade-in ticket form updated

--- fileform.php
+++ fileform.php
@@ -9,13 +9,34 @@
 
     <body>
         <div class="backgroundmain">
-            <div class="navbar">
+            <!--<div class="navbar">
                 <img src="images/Logo.png" class="Logo img-fluid">
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="about.php">About</a></li>
                 </ul>
-            </div>
+            </div>-->
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <img src="images/Logo.png" class="Logo img-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Admin</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
             <div class="content4">
                 <h2><b>File A Report</b></h2>
